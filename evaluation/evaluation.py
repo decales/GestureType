d4f9da57bb4448f992d0720b9670fdc3180589c4
Declan <declan.49@hotmail.com>
2024-04-11 18:23:00 -0600

    added context menu in evaluation application

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -11,10 +11,10 @@ def onEdit(event):
     global index, startTime
     currentText = textBox.get("1.0", "end-1c").strip()
     if currentText == text.text[index]:
-        index += 1
         elapsedTime = time.time() - startTime
-        print("Trial {} complete in {:.2f} seconds".format(index + 1, elapsedTime))
-        prompt.config(text=instructions.instructions[index])
+        print("Trial {} complete in {:.2f} seconds".format(index, elapsedTime))
+        index += 1
+        prompt.config(text=instructions.instructions[index][0])
         startTime = time.time()
 
 
@@ -24,17 +24,28 @@ def onStart():
     textBox.pack(padx=20, pady=20)
     prompt.pack()
     startTime = time.time()
+    textBox.focus_set()
+    textBox.mark_set("insert", "1.0")
+
+
+def toggleContextMenu(event):
+    contextMenu.tk_popup(event.x_root, event.y_root)
 
 
 root = tk.Tk()
 root.geometry("800x250")
 root.title("GestureType Evaluation")
 
+contextMenu = tk.Menu(root, tearoff=0)
+contextMenu.add_command(label="Cut", command=lambda: textBox.event_generate('<<Cut>>'))
+contextMenu.add_command(label="Paste", command=lambda: textBox.event_generate('<<Paste>>'))
+
 textBox = tk.Text(root, height=4, width=50, wrap=tk.WORD)
 textBox.insert("1.0", text.base)
 textBox.bind("<KeyRelease>", onEdit)
+textBox.bind("<Button-3>", toggleContextMenu)
 
-prompt = tk.Label(root, text=instructions.ins1)
+prompt = tk.Label(root, text=instructions.ins1[0])
 
 startButton = tk.Button(root, text="Start", command=onStart)
 startButton.pack(pady=20)
